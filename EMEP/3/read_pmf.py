e99d8976969f1c3acb0957aaa8abeeb261292c7f
Daniel Heinesen <danielh@met.no>
2024-05-08 18:40:35 +0200

    2 is done. 3 needs testing. 4 is just started

---
+++ EMEP/3/read_pmf.py
@@ -0,0 +1,25 @@
+import pyaerocom as pya
+import matplotlib.pyplot as plt
+
+from pyaerocom.io.pyaro.pyaro_config import PyaroConfig
+
+print(pya.const.CACHEDIR)
+# breakpoint()
+
+data_id = "nilupmfebas"
+url = "/home/danielh/Downloads/EIMPs_winter2017-2018_data/EIMPs_winter_2017_2018_ECOC_Levo/"
+
+config = PyaroConfig(
+    name="pmf",
+    data_id=data_id,
+    filename_or_obj_or_url=url,
+    filters={"variables": {"include": ["#elemental_carbon#ug C m-3"]}},
+    name_map={"#elemental_carbon#ug C m-3": "concCec"},
+)
+
+rp = pya.io.ReadUngridded(configs=[config])
+
+data = rp.read()
+
+stations = data.to_station_data_all()
+breakpoint()
